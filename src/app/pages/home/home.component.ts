4cc6601d1c0522094c87986ccb9e16f3a6539c59
Vitor Rodrigues <vitor.rodriguess3112@gmail.com>
2025-06-06 02:15:26 -0300

    features e sobre

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { RouterModule } from '@angular/router';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [CommonModule, ButtonModule, RouterModule], 
+  imports: [CommonModule, ButtonModule, RouterModule, DividerModule], 
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
